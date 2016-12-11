4b90a97e359621064839d20c43bc028c4eedbaa9
Tony Bowden <tony@tmtm.com>
2016-12-11 07:27:47 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,9 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ro.wikipedia.org/wiki/Legislatura_2012-2016_(Camera_Deputaților)',
-  xpath: '//table[.//tr[th[.="Partid"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//tr[th[.="Partid"]]]//tr[td]//td[2]//a[not(@class="new")]/@title'
 )
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { ro: names }, output: false)
