e82f4a897165eadfa7172c1739d65fbe486859b6
Tony Bowden <tony@tmtm.com>
2018-06-20 10:26:08 +0100

    Also fetch via P39s

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,9 @@
 
 require 'wikidata/fetcher'
 
+query = 'SELECT DISTINCT ?item { ?item wdt:P39/wdt:P279* wd:Q17556530 }'
+ids = EveryPolitician::Wikidata.sparql(query)
+
 names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ro.wikipedia.org/wiki/Legislatura_2012-2016_(Camera_Deputaților)',
   xpath: '//table[.//tr[th[.="Partid"]]]//tr[td]//td[2]//a[not(@class="new")]/@title'
@@ -10,4 +13,4 @@ names = EveryPolitician::Wikidata.wikipedia_xpath(
 
 by_cat = WikiData::Category.new('Categorie:Deputați români 2012-2016', 'ro').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { ro: names | by_cat })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { ro: names | by_cat })
