70dc9a0ae938ebb60924207a6ca5be3cb02ee576
Tony Bowden <tony@tmtm.com>
2018-10-15 10:17:34 +0100

    Update for 2016 term

--- scraper.rb
+++ scraper.rb
@@ -6,11 +6,19 @@ require 'wikidata/fetcher'
 query = 'SELECT DISTINCT ?item { ?item wdt:P39/wdt:P279* wd:Q17556530 }'
 ids = EveryPolitician::Wikidata.sparql(query)
 
-names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://ro.wikipedia.org/wiki/Legislatura_2012-2016_(Camera_Deputaților)',
-  xpath: '//table[.//tr[th[contains(.,"Partid")]]]//tr[td]//td[2]//a[not(@class="new")]/@title'
+wd2012 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://ro.wikipedia.org/wiki/Legislatura_2012-2016_(Camera_Deputa%C8%9Bilor)',
+  xpath: '//table[.//tr[th[contains(.,"Partid")]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+  as_ids: true,
 )
 
-by_cat = WikiData::Category.new('Categorie:Deputați români 2012-2016', 'ro').member_titles
+wd2016 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://ro.wikipedia.org/wiki/Legislatura_2016-2020_(Camera_Deputa%C8%9Bilor)',
+  xpath: '//table[.//tr[th[contains(.,"Grup parl")]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+  as_ids: true,
+)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { ro: names | by_cat })
+cat_2012 = WikiData::Category.new('Categorie:Deputați români 2012-2016', 'ro').member_titles
+cat_2016 = WikiData::Category.new('Categorie:Deputați români 2016-2020', 'ro').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids | wd2012 | wd2016, names: { ro: cat_2012|cat_2016 })
